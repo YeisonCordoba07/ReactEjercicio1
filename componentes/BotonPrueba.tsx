a0850fbcb4c1059314dab428af036d9a1cee8e21
YeisonCordoba07 <yalexander.cordoba@gmail.com>
2023-09-17 03:49:27 -0500

    crea estructura inicial

---
+++ componentes/BotonPrueba.tsx
@@ -0,0 +1,13 @@
+interface ButtonProps {
+    text: string;
+  }
+  
+  const Button = ({ text }: ButtonProps) => {
+    return (
+      <button className='bg-white text-black rounded-full px-3 py-1 font-semibold hover:scale-105'>
+        {text}
+      </button>
+    );
+  };
+  
+  export { Button };
---
+++ pages/index.tsx
@@ -0,0 +1,51 @@
+
+const Index = () => {
+  return (
+    <main>
+
+      <aside>
+
+        <div>
+          <img src="" alt="" />
+          <span>Rayan Adlardard</span>
+          <p>Front-End Developer</p>
+        </div>
+
+        <div>
+
+        </div>
+
+        <div>
+          <span>Languages</span>
+
+        </div>
+
+        <div>
+          <span>Programming Languages</span>
+
+        </div>
+
+        <div>
+          <span>Extra Skills</span>
+        </div>
+
+      </aside>
+
+      <section>
+
+
+
+      </section>
+
+      <aside>
+
+        <span>Links</span>
+        
+
+      </aside>
+
+    </main>
+  );
+};
+
+export default Index;
