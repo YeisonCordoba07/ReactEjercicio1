4f97e7f032e1f579dca0671a55e36ab349e9801e
YeisonCordoba07 <yalexander.cordoba@gmail.com>
2023-09-21 20:31:36 -0500

    termina aside redes social y agrega foto de perfil

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,18 +3,24 @@ import {ImFacebook, ImLinkedin2, ImYoutube, ImDribbble} from 'react-icons/im';
 import {BsInstagram, BsTwitter} from 'react-icons/bs';
 
 import {BotonRedes} from '@/componentes/BotonRedes';
+import Image from 'next/image';
 
 
 const Index = () => {
   return (
-    <main>
+    <main className='flex'>
 
-      <aside>
+      <aside className='debug w-80 px-10'>
 
-        <div>
-          <img src="" alt="" />
-          <span>Rayan Adlardard</span>
-          <p>Front-End Developer</p>
+        <div className='flex flex-col items-center gap-3 pt-10 pb-12 border-solid border-b border-gray-200'>
+          <Image className='rounded-full' 
+          src='/YeisonIA.png' 
+          height={150} 
+          width={150} 
+          alt='Foto de perfil'/>
+
+          <span className='text-lg font-medium'>Rayan Adlardard</span>
+          <p className='texto-normal-gris'>Front-End Developer</p>
         </div>
 
         <div>
@@ -37,17 +43,18 @@ const Index = () => {
 
       </aside>
 
-      <section>
+      <section className='debug w-2/3'>
 
 
 
       </section>
 
-      <aside>
+      <aside className='debug w-24 flex flex-col items-center pt-20'>
 
-        <span>Links</span>
+        <span className='mb-4 text-lg font-bold'>Links</span>
 
-        <ul className='flex-col'>
+        <ul className='flex flex-col justify-center gap-4'>
+          
           <li className='fondo-redes bg-blue-500' ><a href='#'><ImFacebook/></a></li>
           <li className='fondo-redes bg-fuchsia-300'><a href='#'><BsInstagram/></a></li>
           <li className='fondo-redes bg-sky-400'><a href='#'><BsTwitter/></a></li>
