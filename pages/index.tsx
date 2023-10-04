c7d746dc7778422a678898e0959ab3431c04e579
YeisonCordoba07 <yalexander.cordoba@gmail.com>
2023-10-03 23:14:44 -0500

    deja fijado aside redes

--- pages/index.tsx
+++ pages/index.tsx
@@ -288,64 +288,72 @@ const Index = () => {
 
 
       <aside className="w-24 flex flex-col items-center pt-10 bg-white">
-        <span className="mb-4 text-lg font-extrabold">Links</span>
 
-        <ul className="flex flex-col justify-center gap-4">
-          <li>
-            <BotonRedes
-              Icono={BsGithub}
-              enlace="https://www.github.com/YeisonCordoba07"
-              colorFondo="rgb(155 155 155)"
-            />
-          </li>
-          <li>
-            <BotonRedes
-              Icono={ImFacebook}
-              enlace="https://www.facebook.com/"
-              colorFondo="rgb(59 130 246)"
-            />
-          </li>
+        <div className="w-24 flex flex-col items-center fixed">
 
-          <li>
-            <BotonRedes
-              Icono={BsInstagram}
-              enlace="https://www.instagram.com/"
-              colorFondo="rgb(240 171 252)"
-            />
-          </li>
+          <span className="mb-4 text-lg font-extrabold">Links</span>
 
-          <li>
-            <BotonRedes
-              Icono={BsTwitter}
-              enlace="https://www.x.com/"
-              colorFondo="rgb(56 189 248)"
-            />
-          </li>
+          <ul className="flex flex-col justify-center gap-4">
+            <li>
+              <BotonRedes
+                Icono={BsGithub}
+                enlace="https://www.github.com/YeisonCordoba07"
+                colorFondo="rgb(155 155 155)"
+              />
+            </li>
+            <li>
+              <BotonRedes
+                Icono={ImFacebook}
+                enlace="https://www.facebook.com/"
+                colorFondo="rgb(59 130 246)"
+              />
+            </li>
 
-          <li>
-            <BotonRedes
-              Icono={ImLinkedin2}
-              enlace="https://www.linkedin.com/"
-              colorFondo="rgb(14 116 144)"
-            />
-          </li>
+            <li>
+              <BotonRedes
+                Icono={BsInstagram}
+                enlace="https://www.instagram.com/"
+                colorFondo="rgb(240 171 252)"
+              />
+            </li>
 
-          <li>
-            <BotonRedes
-              Icono={ImYoutube}
-              enlace="https://www.youtube.com/"
-              colorFondo="rgb(220 38 38)"
-            />
-          </li>
+            <li>
+              <BotonRedes
+                Icono={BsTwitter}
+                enlace="https://www.x.com/"
+                colorFondo="rgb(56 189 248)"
+              />
+            </li>
 
-          <li>
-            <BotonRedes
-              Icono={ImDribbble}
-              enlace="https://www.dribbble.com/"
-              colorFondo="rgb(249 115 22)"
-            />
-          </li>
-        </ul>
+            <li>
+              <BotonRedes
+                Icono={ImLinkedin2}
+                enlace="https://www.linkedin.com/"
+                colorFondo="rgb(14 116 144)"
+              />
+            </li>
+
+            <li>
+              <BotonRedes
+                Icono={ImYoutube}
+                enlace="https://www.youtube.com/"
+                colorFondo="rgb(220 38 38)"
+              />
+            </li>
+
+            <li>
+              <BotonRedes
+                Icono={ImDribbble}
+                enlace="https://www.dribbble.com/"
+                colorFondo="rgb(249 115 22)"
+              />
+            </li>
+
+            
+          </ul>
+
+        </div>
+        
         
       </aside>
       <Dialogo open={cambiarDialogo} setCerrarDialogo={setCambiarDialogo}/>
